aa7d7d924076e911cdd82d2a0a434ff694324428
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-12-15 13:28:32 +0100

    clean training pipeline in main

--- babyLLM/main.py
+++ babyLLM/main.py
@@ -11,6 +11,8 @@ import argparse
 import pandas as pd
 from termcolor import colored
 from collections import Counter
+import logging
+from torch.utils.tensorboard import SummaryWriter
 
 from torch.utils.data import Dataset
 import numpy as np
@@ -32,58 +34,58 @@ class WikiTalkDataset(Dataset):
         return item
 
 
-def get_vocab_size():
+def get_vocab_info(data):
     '''
-        To be implemented.
+        Create a vocabulary mapping with training data text processed as a bag of words. Default vocab_size is the number of different words in the whole dataset.
+        TODO add a feature to choose a vocab size (in order to alleviate data processing for bigger datasets). This might be required for open web corpus.
     '''
-    pass
+    # here are all the unique characters that occur in this text
+    chars = sorted(list(set(data)))
+    vocab_size = len(chars)
+    # create a mapping from characters to integers
+    stoi = { ch:i for i,ch in enumerate(chars) }
+    itos = { i:ch for i,ch in enumerate(chars) }
+    return vocab_size, stoi, itos
 
-args = {
-    'vocab_size':get_vocab_size(), # to be implemented
-    'batch_size':16, # how many independent sequences will we process in parallel?
-    'block_size':32, # what is the maximum context length for predictions?
-    'max_iters':5000,
-    'eval_interval':100,
-    'lr':1e-3,
-    'device':activate_gpu(),
-    'eval_iters':200,
-    'n_embd':64,
-    'n_head':4,
-    'n_layer':4,
-    'dropout':0.0
-}
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
+
 # Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
+def train_val_split(data, train_ratio=0.9):
+    '''
+        Processes train/val split according to the given train_ratio (default = 90% of the dataset is used for training)
+    '''
+    tensor_data = torch.tensor(encode(data), dtype=torch.long)
+    n = int(train_ratio*len(tensor_data))
+    train_data = tensor_data[:n]
+    val_data = tensor_data[n:]
+    return train_data, val_data
+
 
 # data loading
 def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
+    '''
+        This function is a kind of manual dataloader.
+    '''
+    # get the desired data split
     data = train_data if split == 'train' else val_data
+    # randomly select some indexes
     ix = torch.randint(len(data) - args.block_size, (args.batch_size,))
+    # deduce corresponding data
     x = torch.stack([data[i:i+args.block_size] for i in ix])
     y = torch.stack([data[i+1:i+args.block_size+1] for i in ix])
+    # send data to device
     x, y = x.to(device), y.to(device)
     return x, y
 
+
 @torch.no_grad()
 def estimate_loss():
+    '''
+        This function is NOT a training loop. It simply estimates the loss value on each batch and outputs the mean of these loss values.
+    '''
     out = {}
     model.eval()
     for split in ['train', 'val']:
@@ -97,44 +99,35 @@ def estimate_loss():
     return out
 
 
-model = BabyLanguageModel()
-device = args.device
-m = model.to(device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+def train_and_infer(model, args, optimizer):
+    for iter in tqdm(range(args.max_iters)):
 
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+        # every once in a while evaluate the loss on train and val sets
+        if iter % args.eval_interval == 0 or iter == args.max_iters - 1:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-for iter in tqdm(range(args.max_iters)):
+        # sample a batch of data
+        xb, yb = get_batch('train')
 
-    # every once in a while evaluate the loss on train and val sets
-    if iter % args.eval_interval == 0 or iter == args.max_iters - 1:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        # evaluate the loss
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
 
-    # sample a batch of data
-    xb, yb = get_batch('train')
-
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-
-# save model
-torch.save(m, './models/babyllm-gptlike.pt')
-
-# generate from the model
-# context = torch.zeros((1, 1), dtype=torch.long, device=device)
-context = torch.tensor(encode("Hey! How are you?"), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=200)[0].tolist()))
 
 if __name__ == "__main__":
 
+    # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+    with open('input.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    vocab_size, stoi, itos = get_vocab_info(text)
+
     # hyperparameters default config
     args = {
-        'vocab_size':get_vocab_size(), # to be implemented
+        'vocab_size':get_vocab_info(text)[0], # to be implemented
         'batch_size':16, # how many independent sequences will we process in parallel?
         'block_size':32, # what is the maximum context length for predictions?
         'max_iters':5000,
@@ -173,3 +166,24 @@ if __name__ == "__main__":
         'n_layer':l,
         'dropout':d
     }
+
+    train_data, val_data = train_val_split(text)
+
+    device = args.device
+    model = BabyLanguageModel()
+    model.to(device)
+    # print the number of parameters in the model
+    print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
+
+    # create a PyTorch optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+
+    train_and_infer(model, args, optimizer)
+
+    # save model
+    torch.save(model, './models/babyllm-gptlike.pt')
+
+    # generate from the model
+    # context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    context = torch.tensor(encode("Hey! How are you?"), dtype=torch.long, device=device)
+    print(decode(model.generate(context, max_new_tokens=200)[0].tolist()))
