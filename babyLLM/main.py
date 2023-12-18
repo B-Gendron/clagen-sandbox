32f83276d3c0293896a9907194ea3eeedeb32039
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-12-18 11:10:12 +0100

    successfully train on openweb data

--- babyLLM/main.py
+++ babyLLM/main.py
@@ -48,17 +48,24 @@ def read_data(folder_name):
     corpus = []
     for file_path in tqdm(file_list):
         with open(file_path, 'r', encoding='utf-8') as f:
-            corpus.append(f.read())
+            text = f.read()
+            corpus.append(text)
     print("Finished!")
-    return corpus
+
+    # gather all textual data in a big string
+    text_data = " ".join(corpus)
+    return text_data
+
 
 def get_vocab_info(data):
     '''
         Create a vocabulary mapping with training data text processed as a bag of words. Default vocab_size is the number of different words in the whole dataset.
+        TODO change vocab size computation (use torch.vocab ?)
         TODO add a feature to choose a vocab size (in order to alleviate data processing for bigger datasets). This might be required for open web corpus.
     '''
     # here are all the unique characters that occur in this text
     chars = sorted(list(set(data)))
+    # chars = sorted(list(set(data)))
     vocab_size = len(chars)
     # create a mapping from characters to integers
     stoi = { ch:i for i,ch in enumerate(chars) }
@@ -144,10 +151,11 @@ def train_and_infer(model, args, optimizer, device):
 
 if __name__ == "__main__":
 
-    text = read_data('tinyshakespeare')
+    # text = read_data('tinyshakespeare')
     text = read_data('openwebtext')
 
     vocab_size, stoi, itos = get_vocab_info(text)
+    print(vocab_size)
 
     # hyperparameters default config
     args = {
