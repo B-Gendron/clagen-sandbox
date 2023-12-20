c05aecf0df9b1311fdee43f7c56518edeb646805
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-12-20 17:26:46 +0100

    add empty script for finetuning

---
+++ babyLM/code/finetune.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from glob import glob
+import os
+import subprocess
+import numpy as np
+from datasets import load_dataset
+from tqdm import tqdm
+import argparse
+import pandas as pd
+from termcolor import colored
+from collections import Counter
+import logging
+from torch.utils.tensorboard import SummaryWriter
+
+from torch.utils.data import Dataset
+import numpy as np
+
+from utils import activate_gpu
+from models import BabyLanguageModel
+
+class WikiTalkDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        item = {
+          ## TODO add appropriate content
+        }
+        return item
+    
