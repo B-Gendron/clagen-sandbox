0face32fb6c1d7337ba4eae636a8b84b31897af5
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2024-01-04 15:50:00 +0100

    store draft functions in draft

--- old/draft.py
+++ old/draft.py
@@ -16,3 +16,38 @@
 
 # encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 # decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# def get_vocab(folder_name):
+# '''
+#     This fuction works, but has been replaced by a more optimized way to both build vocabulary and tokenize data.
+    
+#     Build a vocabulary of type torch.Vocab from the text dataset stored in folder_name folder.
+    
+#     @param folder_name (str): the name of the folder where data is stored. Expects a data.txt file.
+
+#     @return vocab (torch.Vocab): the inferred vocabulary, with lowercase tokens, excluding empty tokens
+# '''
+# file_path = os.path.join(f'../{folder_name}', "data.txt")
+
+# def yield_tokens():
+#     with open(file_path, 'r', encoding='utf-8') as f:
+#         # yield tokens from each line
+#         for line in f:
+#             # get initial words that are not empty words
+#             words = [ w for w in line.strip().split() if w != ' ']
+#             # make simple tokens by lowering words
+#             tokens = list(map(lambda x: x.lower(), words))
+#             yield tokens
+
+# # build the vocab using the generator
+# token_generator = yield_tokens()
+# print(type(token_generator))
+# vocab = build_vocab_from_iterator(token_generator, specials=["<unk>"])
+
+# return vocab
+    
+# def read_tinyshakepeare():
+#     # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+#     with open('input.txt', 'r', encoding='utf-8') as f:
+#         text = f.read()
+#     return text
