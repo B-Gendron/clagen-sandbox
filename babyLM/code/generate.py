d9b0ded0de0ccbd649ba8fa2a6b61e6cb02692ab
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2024-01-08 17:03:59 +0100

    implement object loading to generate from babyLM

--- babyLM/code/generate.py
+++ babyLM/code/generate.py
@@ -11,21 +11,18 @@ from torchtext.vocab import build_vocab_from_iterator
 from torchtext.data import get_tokenizer
 tokenize = get_tokenizer("basic_english")
 
-
 from train import *
 
 
-def generate_from_prompt(prompt_text):
+def generate_from_prompt(prompt_text, model, device='cpu', block_size=32):
     print(colored('Generating from a simple prompt', 'green'))
-    prompt = torch.tensor(encode(vocab, prompt_text), dtype=torch.long, device=args['device']).unsqueeze(-1)
+    prompt = torch.tensor(encode(vocab, prompt_text), dtype=torch.long, device=device).unsqueeze(-1)
     print(f'Prompt: {prompt_text}')
-    print(decode(model.generate(prompt, max_new_tokens=200, block_size=args['block_size'])[0].tolist()))
+    print(decode(model.generate(prompt, max_new_tokens=200, block_size=block_size)[0].tolist()))
 
 
 if __name__ == '__main__':
-        
-    model = torch.load('../models/babyllm-gptlike.pt')
-    all_tokens = get_data('openwebtext')
-    vocab = build_vocab_from_iterator(token_generator(), specials=["<unk>"], special_first=True)
 
-    generate_from_prompt("Hey, how are you?")
+    vocab = torch.load('../objects/vocab.pt', map_location=torch.device('cpu'))
+    model = torch.load('../models/babyllm-gptlike.pt', map_location=torch.device('cpu'))
+    generate_from_prompt("Hey, how are you?", model=model)
