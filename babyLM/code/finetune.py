158219916e8fffd6f65ef460d90476893374abbb
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2024-01-18 14:57:32 +0100

    start to build dataset class

--- babyLM/code/finetune.py
+++ babyLM/code/finetune.py
@@ -10,6 +10,7 @@ import os
 import subprocess
 import numpy as np
 from datasets import load_dataset
+from torch.utils.data import Dataset
 from tqdm import tqdm
 import argparse
 import pandas as pd
@@ -22,16 +23,25 @@ from torch.utils.tensorboard import SummaryWriter
 from utils import activate_gpu
 from models import BabyLanguageModel
 
-class WikiTalkDataset(Dataset):
+class WikiTalkDataset(torch.utils.data.Dataset):
+    '''
+        Dataset class for wikitalk pages dataset
+    '''
     def __init__(self, data):
-        self.data = data
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
         item = {
-          ## TODO add appropriate content
+            'dial_id':      np.array(self._data[idx]['dial_id']),
+            'utt_id':       np.array(self._data[idx]['utt_id']), # penser à padder ça éventuellement
+            'embedding':    np.array(self._data[idx]['embedding'])
         }
         return item
     
