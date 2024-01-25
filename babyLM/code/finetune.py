68237e2afa6a6fed740b3f5bab9751c482d11b68
Barbara Gendron <bagendron@fnancy>
2024-01-25 15:14:21 +0100

    start implementing testing loop

--- babyLM/code/finetune.py
+++ babyLM/code/finetune.py
@@ -65,7 +65,7 @@ def get_args_and_dataloaders(dataset, dataset_class):
         @return val_loader (dataloader):    the dataloader that contains the validation samples
         @return test_loader (dataloader):   the dataloader that contains the test samples
     '''
-    args = {'train_bsize': 1, 'eval_bsize': 1, 'lr': 0.00001}
+    args = {'train_bsize': 8, 'eval_bsize': 1, 'lr': 0.00001}
     train_loader = DataLoader(dataset=dataset_class(dataset["train"], args=args), pin_memory=True, batch_size=args['train_bsize'], shuffle=True, drop_last=True)
     val_loader   = DataLoader(dataset=dataset_class(dataset["validation"], args=args), pin_memory=True, batch_size=args['eval_bsize'], shuffle=True, drop_last=True)
     test_loader  = DataLoader(dataset=dataset_class(dataset["test"], args=args), pin_memory=True, batch_size=args['eval_bsize'], shuffle=True, drop_last=True)
@@ -96,6 +96,7 @@ def train(args, model, train_loader, stoi, itos, optimizer, epoch):
         optimizer.zero_grad()
 
         batch_trues, batch_preds = [], [] 
+        batch_ids = []
 
         # remove padded part
         for idx in range(args['train_bsize']):
@@ -111,26 +112,32 @@ def train(args, model, train_loader, stoi, itos, optimizer, epoch):
                     dialog_without_pad.append(utterance_without_pad)
 
             if dialog_without_pad != []:
+                dial_id = batch['dial_id'][idx].item()
+                batch_ids.append(dial_id)
                 # dump dialog encoding without padding in a json file
                 with open(f'../objects/batch_{idx}.json', 'w') as f:
-                    json.dump({'dial_id':batch['dial_id'][idx].item(), 'dial_encoding':dialog_without_pad}, f, indent=2)
+                    json.dump({'dial_id':dial_id, 'dial_encoding':dialog_without_pad}, f, indent=2)
 
         # make a list with all the file names
         file_list = [os.path.join("../objects/", filename) for filename in [f"batch_{i}.json" for i in range(args['train_bsize'])]]
         for f in file_list:
             prompt, label = get_prompt_and_label(f, 'train', stoi, args['device'])
             read_level_probas = model.predict_readability_levels(prompt, block_size=args['block_size'])
-            # print(read_level_probas)
             batch_preds.append(read_level_probas)
-            # output = pretrained_model.generate(prompt, max_new_tokens=1, block_size=args['block_size'])[0].tolist()
-            # predicted_class = parse_output_and_deduce_class(output, itos)
             # update trues/preds lists
             batch_trues.append(label)
-            preds.append(torch.argmax(read_level_probas).item())
-            # print(f'Batch trues: {batch_trues}, Trues: {trues}, Preds: {preds}')
+        # convert to tensors
+        batch_preds, batch_trues = torch.stack(batch_preds), torch.tensor(batch_trues)
+
+        # these 3 elements should be saved in a file during training
+        # print(batch_trues)
+        # print(batch_preds)
+        # print(torch.argmax(batch_preds, dim=-1))
+        save_batch_info(batch_ids, batch_trues, torch.argmax(batch_preds, dim=-1), batch_preds, output_file='first_test')
+        preds.extend(torch.argmax(batch_preds, dim=-1).tolist()) # this should not work.
 
         # compute and backpropagate MSE loss on batch predictions
-        loss = ce_loss(torch.stack(batch_preds), torch.tensor(batch_trues))
+        loss = ce_loss(batch_preds.to(device), batch_trues.to(device))
         loss_it.append(loss.item())
         loss.backward()
         optimizer.step()
@@ -141,25 +148,89 @@ def train(args, model, train_loader, stoi, itos, optimizer, epoch):
     loss_it_avg = sum(loss_it)/len(loss_it)
 
     # print useful information about the training progress and scores on this training set's full pass
-    print("Epoch %s/%s - %s : (%s %s)" % (colored(str(epoch+1), 'blue'),args['max_eps'] , colored('Training', 'blue'), colored('Average loss: ', 'cyan'), sum(loss_it)/len(loss_it)))
+    print("Epoch %s/%s - %s : (%s %s)" % (colored(str(epoch+1), 'blue'),args['max_eps'] , colored('Training', 'blue'), colored('Average loss: ', 'cyan'), loss_it_avg))
 
 	# 🛑 add some metrics to keep with a label and the epoch index
     writer.add_scalar("Loss/train", loss_it_avg, epoch)
 
-    return loss_it_avg
+    return loss_it_avg, trues, preds
 
+def test(args, model, loader, target):
+    model.eval()
+    loss_it = []
+    device = args['device']
+    writer = args['writer']
+    ce_loss = nn.CrossEntropyLoss()
+    preds, trues = [], []
+
+    for it, batch in tqdm(enumerate(loader), desc="%s:" % (target), total=loader.__len__()):
+
+        with torch.no_grad():
+            batch = {'dial_id': batch['dial_id'].to(device), 'utt_id': batch['utt_id'].to(device), 'embedding' : batch['embedding'].to(device)}
+
+            batch_trues, batch_preds = [], [] 
+
+            # remove padded part
+            for idx in range(args['train_bsize']):
+                # get the dialog data 
+                encoded_dialog = batch['embedding'][idx]
+                dialog_without_pad = []
+                for utterance in encoded_dialog:
+                    # select token indexes only
+                    utt = utterance.tolist()
+                    utterance_without_pad = utt[:utt.index(-1) if -1 in utt else len(utt)]
+                    # naturally remove the utterances full of pad
+                    if utterance_without_pad != []:
+                        dialog_without_pad.append(utterance_without_pad)
+
+                if dialog_without_pad != []:
+                    # dump dialog encoding without padding in a json file
+                    with open(f'../objects/batch_{idx}.json', 'w') as f:
+                        json.dump({'dial_id':batch['dial_id'][idx].item(), 'dial_encoding':dialog_without_pad}, f, indent=2)
+
+            # make a list with all the file names
+            file_list = [os.path.join("../objects/", filename) for filename in [f"batch_{i}.json" for i in range(args['train_bsize'])]]
+            for f in file_list:
+                prompt, label = get_prompt_and_label(f, 'train', stoi, args['device'])
+                read_level_probas = model.predict_readability_levels(prompt, block_size=args['block_size'])
+                # update trues/preds lists
+                batch_preds.append(read_level_probas)
+                batch_trues.append(label)
+                preds.extend(torch.argmax(read_level_probas, dim=-1).tolist())
+
+            # compute and backpropagate MSE loss on batch predictions
+            loss = ce_loss(torch.stack(batch_preds).to(device), torch.tensor(batch_trues).to(device))
+            loss_it.append(loss.item())
+
+            # update general lists
+            trues.extend(batch_trues)
+
+        loss_it_avg = sum(loss_it)/len(loss_it)
+
+        # print useful information about the training progress and scores on this training set's full pass
+        print("%s : (%s %s)" % (colored(f'{target}', 'blue'), colored('Average loss: ', 'cyan'), loss_it_avg))
+
+        # 🛑 add some metrics to keep with a label and the epoch index
+        writer.add_scalar(f"Loss/{target}", loss_it_avg)
+
+        return loss_it_avg, trues, preds
+
+
+def run_epochs(args, model, train_loader, val_loader, test_loader, optimizer):
+    pass
 
 if __name__ == "__main__":
 
     wikitalk = load_from_disk("../wikitalk")
     args, train_loader, val_loader, test_loader = get_args_and_dataloaders(wikitalk, WikiTalkDataset)
-    args.update({'vocab_size':239267, # new vocab size corresponding to the new dataset
+    args.update({'vocab_size':239270, # new vocab size corresponding to the new dataset + add 3 onto concepts
                 'batch_size':8,
                 'block_size':64, 
                 'max_iters':5000,
                 'eval_interval':100,
                 'lr':1e-3,
-                'device':activate_gpu(force_cpu=True),
+                'device':activate_gpu(),
+                'max_eps':10,
                 'eval_iters':1000,
                 'n_embd':64,
                 'n_heads':8,
@@ -170,12 +241,12 @@ if __name__ == "__main__":
 
     print("Getting stoi and itos dicts...")
     itos, stoi = load_vocab_mappings()
+    print(len(itos))
 
     print("Load the pretrained model weights...")
-    # model_path = '../models/babyllm-gptlike_64_22012024223644_nq_params.pt'
-    model_path = '../models/babyllm-gptlike_64_22012024223644_nq_params.pt'
+    model_path = '../models/babyllm-gptlike_64_25012024110257_nq_params.pt'
     pretrained_model = BabyLanguageModel(args)
-    pretrained_model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    pretrained_model.load_state_dict(torch.load(model_path))
     pretrained_model.to(args['device'])
 
     print("Start fine-tuning on one epoch...")
